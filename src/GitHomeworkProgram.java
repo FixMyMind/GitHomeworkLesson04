23127e83c97d25cdcc3eda6834fd657321bfc18e
Oleh Lukianov <oleg.lukianov@gmail.com>
2024-03-04 21:15:57 +0200

    Second commit.

--- src/GitHomeworkProgram.java
+++ src/GitHomeworkProgram.java
@@ -1,5 +1,6 @@
 public class GitHomeworkProgram {
     public static void main(String[] args) {
         System.out.println("Hello my new git repository!");
+        System.out.println("It`s my second commit!!!");
     }
 }
